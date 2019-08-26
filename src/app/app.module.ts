e517bb52d492165cabadda39ab86cf758f322cfe
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2019-08-26 06:59:04 +0200

    added a questions & welcome components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,26 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { QuestionFormComponent } from './question-form/question-form.component';
 import { ResultsComponent } from './results/results.component';
+import { RouterModule, Routes } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+
+const appRoutes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  { path: ':quizId', component: QuestionsComponent },
+  { path: '', redirectTo: "welcome", pathMatch: "prefix" },
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionFormComponent,
-    ResultsComponent
+    ResultsComponent,
+    QuestionsComponent,
+    WelcomeComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     HttpClientModule,
     ReactiveFormsModule
